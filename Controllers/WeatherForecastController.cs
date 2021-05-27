3301c376ee164a136fbef87a47bdb2215bd16172
gabrielnascimentosilva <gabriel.nascimento@soitic.com>
2021-05-27 20:12:46 -0300

    Tarefa1 alterado controller

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Tarefa1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Congelando", "Estimulante", "Frio", "Frio", "Suave", "Quente", "Ameno", "Quente", "Sufocante", "Ardente"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-35, 60), //temperatura em celsius randomica
+                Summary = Summaries[rng.Next(Summaries.Length)] //randomicamente o sumario que agr está em portugues
+            })
+            .ToArray();
+        }
+    }
+}
