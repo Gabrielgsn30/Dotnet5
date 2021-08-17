7f5f73d6a22f727d42060a6f5b191dcc10b90fd9
gabrielnascimentosilva <gabriel.nascimento@soitic.com>
2021-08-16 21:11:10 -0300

    incluindo todos os testes para diretor seguindo as regras dos DTOs

--- MeuAppTests/UnitTest1.cs
+++ MeuAppTests/UnitTest1.cs
@@ -7,10 +7,28 @@ namespace MeuAppTests
     public class UnitTest1
     {
         [Fact]
-        public void NomeDiretorAprentarErroSeForVazio()
+        public void NomeDiretorAprentarErroSeForNullo()
         {
             var validator = new DiretorInputPostDTOValidator();
             var dto = new DiretorInputPostDTO { Nome = null };
+            var result = validator.TestValidate(dto);
+            result.ShouldHaveValidationErrorFor(diretor => diretor.Nome);
+        }
+
+        [Fact]
+        public void NomeDiretorAprentarErroSeForVazio()
+        {
+            var validator = new DiretorInputPostDTOValidator();
+            var dto = new DiretorInputPostDTO { Nome = "" };
+            var result = validator.TestValidate(dto);
+            result.ShouldHaveValidationErrorFor(diretor => diretor.Nome);
+        }
+
+        [Fact]
+        public void NomeDiretorAprentarErroSeForGrande()
+        {
+            var validator = new DiretorInputPostDTOValidator();
+            var dto = new DiretorInputPostDTO { Nome = "11111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111" };
             var result = validator.TestValidate(dto);
             result.ShouldHaveValidationErrorFor(diretor => diretor.Nome);
         }
